e65660f53e4f6d4a1345d38c21a4299164f468fb
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-04-29 14:13:57 +0530

    section 1 done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
 let menuicon = document.querySelector(".menu-icon");
 let menulist = document.querySelector(".menu-list1");
+let line2 = document.querySelector(".line2");
+let line1 = document.querySelector(".line1");
+let line3 = document.querySelector(".line3");
 
 menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("-right-full");
+    menulist.classList.toggle("right-0");
     menuicon.classList.toggle("active-icon1");
-    menulist.classList.toggle("show-list");
+    line2.classList.toggle("hidden");
+    line1.classList.toggle("bg-[#F36A3E]");
+    line1.classList.toggle("bg-black");
+    line3.classList.toggle("bg-[#F36A3E]");
+    line3.classList.toggle("bg-black");
 });
